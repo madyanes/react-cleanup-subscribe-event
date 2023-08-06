ac7b9e8ccf9bb0f2228ccb0ea92081c80e8e1895
Madyan Eka S <110826582+madyanes@users.noreply.github.com>
2023-08-06 16:56:46 +0700

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,18 @@
+function subscribeGitHubProfile(username, listener) {
+  console.count(`subscribe for ${username}`);
+
+  // mock "subscription" ...
+  const intervalId = setInterval(async () => {
+    console.count(`listener for ${username} called`);
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const profile = await response.json();
+    listener(profile);
+  }, 3000);
+
+  return () => {
+    console.count(`unsubscribe for ${username}`);
+    clearInterval(intervalId);
+  };
+}
+
+export { subscribeGitHubProfile };
